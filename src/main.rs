a0dc6562bac62227a31a87e8f627478b965b1b98
Yichangcs <yccsml@163.com>
2020-07-08 18:01:05 +0800

    second try

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,9 @@ fn main() -> io::Result<()> {
 
     io::stdin().read_line(&mut line)?;
     
-    let mut statement = vec![];
+    let mut iter = line.split_whitespace();
 
-    for item in line.trim().chars() {
-        if item != ' ' {
-            statement.push(item);
-        }
-    }
-
-   for item in statement {
-       println!("-> {}", item);
-   }
+    let mut cin = iter.next().unwrap();
 
     Ok(())
 }
@@ -36,7 +28,53 @@ struct Token {
     value: f32,
 }
 
+struct CIN {
+    index: usize,
+    stream: String, 
+}
 
+
+impl CIN {
+    fn new(s: String) -> CIN {
+        CIN {
+            index: 0,
+            stream: s,
+        }
+    }
+
+    fn putback(&mut self) {
+        if self.index > 0 {
+            self.index = self.index - 1;
+        } else {
+            panic!("nothing to putback");
+        }
+    }
+
+    fn cin2val(&mut self) -> f32 {
+   
+        let length = self.stream.len();
+
+        let current_str = &self.stream[self.index..length];
+
+        let mut count = 0; 
+
+        for c in current_str.chars() {
+            if c.is_numeric() || c == '.' {
+                count += 1;
+            } else {
+                break
+            }
+        } 
+        
+        let valstr = &current_str[self.index..self.index+count];
+        
+        self.index = self.index + count;
+
+        valstr.parse::<f32>().unwrap()
+
+    }
+
+}
 impl Token {
     fn new(c: Op, v: f32) -> Token {
         Token {
